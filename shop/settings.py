5ed901c6022e3708deb79175f6d6d6c7428018e1
KseniaShkolnikova <isip_k.v.shkolnikova@mpt.ru>
2025-06-19 17:11:28 +0300

    s

--- shop/settings.py
+++ shop/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
-        'NAME': 'sushi',
-        'USER':'postgres',
-        'PASSWORD':'1'
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'',
+        'PORT':'3306',
+        'NAME': '$sushi',
+        'USER':'',
+        'PASSWORD':''
     }
 }
 
